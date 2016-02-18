bd6f78ae7265d0b4eacbe8f5ee3c5b13d9f63887
Deng Haijun <hbdhj@sina.com>
2016-02-18 12:04:41 +0800

    change structure

---
+++ codewars_python/8.py
@@ -0,0 +1,9 @@
+def knapsack(capacity, items):
+    # Be greedy!
+    return []
+
+knapsack(100, ((1, 1),));
+knapsack(100, ((100, 1),));
+knapsack(100, ((1, 1),(3, 4)));
+knapsack(100, ((60, 80),(50, 50)));
+knapsack(100, ((10, 10),(30, 40),(56, 78)));
