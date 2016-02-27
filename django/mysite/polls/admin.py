aced4959988c69c264c1d2598dee0c048d6c4d02
Deng Haijun <hbdhj@sina.com>
2016-02-27 20:48:46 +0800

    django polls web app basic part 7

--- django/mysite/polls/admin.py
+++ django/mysite/polls/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
