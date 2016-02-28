94f41cfd64695778f3ce350d2b2ea0a88afc5c38
Deng Haijun <hbdhj@sina.com>
2016-02-28 19:56:46 +0800

    jamesonquave Swift Tutorial – Developing iOS Apps Part 3
    refectory
    not work on iTunes because ATS

---
+++ Swift_iOS/HelloWorld/HelloWorld/APIController.swift
@@ -0,0 +1,59 @@
+//
+//  APIController.swift
+//  HelloWorld
+//
+//  Created by denghaijun on 16/2/28.
+//  Copyright © 2016年 denghaijun. All rights reserved.
+//
+
+import Foundation
+
+protocol APIControllerProtocol {
+    func didReceiveAPIResults(results: NSArray)
+}
+
+class APIController {
+    var delegate: APIControllerProtocol?
+    
+    func searchItunesFor(searchTerm: String) {
+        
+        // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
+        let itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+        
+        // Now escape anything else that isn't URL-friendly stringByAddingPercentEncodingWithAllowedCharacters
+        if let escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding) {
+            //if let escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEncodingWithAllowedCharacters() {
+            //{
+            let urlPath = "http://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
+            //let urlPath = "http://localhost/json.php"
+            let url = NSURL(string: urlPath)
+            let session = NSURLSession.sharedSession()
+            
+            let task = session.dataTaskWithURL(url!, completionHandler: {data, response, error -> Void in
+                print("Task completed")
+                
+                do {
+                    if let jsonResult = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as? NSDictionary {
+                        print(jsonResult)
+                        if let results: NSArray = jsonResult["results"] as? NSArray {
+                        //if let results: NSArray = jsonResult["actions"] as? NSArray {
+                            dispatch_async(dispatch_get_main_queue(), {
+                                //self.tableData = results
+                                //self.appsTableView!.reloadData()
+                                self.delegate?.didReceiveAPIResults(results)
+                            })
+                        }
+                    }
+                }
+                catch
+                {
+                    print("Something went wrong!")
+                }
+            })
+            
+            // The task is just an object with all these properties set
+            // In order to actually make the web request, we need to "resume"
+            task.resume()
+        }
+    }
+}
---
+++ Swift_iOS/HelloWorld/HelloWorld/SearchResultsViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by denghaijun on 16/2/28.
+//  Copyright © 2016年 denghaijun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, APIControllerProtocol {
+
+    @IBOutlet weak var appsTableView: UITableView!
+    var tableData = []
+    let api = APIController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        api.delegate = self
+        api.searchItunesFor("google")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section:    Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        //print(indexPath.row)
+        //print(self.tableData[indexPath.row])
+        if let rowData: NSDictionary = self.tableData[indexPath.row] as? NSDictionary,
+            // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
+            urlString = rowData["artworkUrl60"] as? String,
+            // Create an NSURL instance from the String URL we get from the API
+            imgURL = NSURL(string: urlString),
+            // Get the formatted price string for display in the subtitle
+            formattedPrice = rowData["formattedPrice"] as? String,
+            // Download an NSData representation of the image at the URL
+            imgData = NSData(contentsOfURL: imgURL),
+            // Get the track name
+            trackName = rowData["trackName"] as? String {
+            //trackName = rowData["Name"] as? String {
+                // Get the formatted price string for display in the subtitle
+                //cell.detailTextLabel?.text = formattedPrice
+                // Update the imageView cell to use the downloaded image data
+                //cell.imageView?.image = UIImage(data: imgData)
+                // Update the textLabel text to use the trackName from the API
+                cell.textLabel?.text = trackName
+        }
+        return cell
+    }
+    
+    func didReceiveAPIResults(results: NSArray) {
+        dispatch_async(dispatch_get_main_queue(), {
+            self.tableData = results
+            self.appsTableView!.reloadData()
+        })
+    }
+}
+
