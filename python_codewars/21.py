02b77ff2f517df1dec7a1208a79365e0aae185de
Deng Haijun <hbdhj@sina.com>
2016-03-27 22:36:11 +0800

    21 pass

--- python_codewars/21.py
+++ python_codewars/21.py
@@ -10,22 +10,27 @@ def next_bigger(n):
             if str_n[i-1]>str_n[i-2] and convert == False:
                 convert = True
                 break
-        ret_list = sorted(str_n[i:]+str_n[i-2])
-        ret= ''.join(ret_list)
-        ret = str_n[:i-2] + str_n[i-1] + ret
-        print(ret)
+        sorted_list=sorted(str_n[i-1:])
+        head_list = str_n[:i-2]
+        for j in range(len(sorted_list)):
+            if sorted_list[j] > str_n[i-2]:
+                head_list+=sorted_list[j]
+                sorted_list[j] = str_n[i-2]
+                break
+        ret= head_list + ''.join(sorted_list)
         if convert:
             return int(ret)
         else:
             return -1
 
-#assert(next_bigger(12)==21)
-#assert(next_bigger(513)==531)
-#assert(next_bigger(2017)==2071)
-#assert(next_bigger(414)==441)
-#assert(next_bigger(144)==414)
-#assert(next_bigger(9)==-1)
-#assert(next_bigger(111)==-1)
-#assert(next_bigger(531)==-1)
-#assert(next_bigger(1234567890)==1234567908)
-assert(next_bigger(59884848493558)==59884848483559)
+assert(next_bigger(12)==21)
+assert(next_bigger(513)==531)
+assert(next_bigger(2017)==2071)
+assert(next_bigger(414)==441)
+assert(next_bigger(144)==414)
+assert(next_bigger(9)==-1)
+assert(next_bigger(111)==-1)
+assert(next_bigger(531)==-1)
+assert(next_bigger(1234567890)==1234567908)
+assert(next_bigger(59884848459853)==59884848483559)
+#59853 83559 93558
--- python_codewars/22.py
+++ python_codewars/22.py
@@ -24,6 +24,7 @@ class User:
             inc_rank = int(self.progress/100)
             #print(inc_rank)
             self.progress = self.progress%100
+            print(self.progress)
             if self.rank<0 and (self.rank+inc_rank)>=0:
                 self.rank+=inc_rank+1
             else:
@@ -34,10 +35,13 @@ class User:
 
 
 user = User()
-print(user.rank) # => -8
-print(user.progress) # => 0
-user.inc_progress(-7)
-print(user.progress) # => 10
-user.inc_progress(-5) # will add 90 progress
+#print(user.rank) # => -8
+#print(user.progress) # => 0
+#user.inc_progress(-7)
+#print(user.progress) # => 10
+#user.inc_progress(-5) # will add 90 progress
+#print(user.progress) # => 0 # progress is now zero
+#print(user.rank) # => -7 # rank was upgraded to -7
+user.inc_progress(-1) # will add 90 progress
 print(user.progress) # => 0 # progress is now zero
 print(user.rank) # => -7 # rank was upgraded to -7
