bb45314f81e936e4d145d3f00c5e4f138c53e957
Deng Haijun <hbdhj@sina.com>
2016-03-28 08:48:06 +0800

    4 pass

--- python_codewars/4.py
+++ python_codewars/4.py
@@ -1,28 +1,37 @@
+# The Enigma Machine - Part 1: The Plugboard
+
 class Plugboard(object):
     board={};
-    def __init__(self, wires):
+    def __init__(self, wires=[]):
         """
         wires: This is the mapping of pairs of characters
         """
+        print(wires)
         if len(wires)>20:
-            print("")
+            raise Exception("Should not have accepted too many wires defined");
+            return;
+        self.board = {}
         counter=0
         for letter in wires:
+            if letter in self.board.keys():
+                raise Exception("Should not have accepted a second definition for a wire end");
+                break
             if counter%2==0:
                 self.board[letter]=wires[counter+1];
             else:
                 self.board[letter]=wires[counter-1];
             counter+=1;
-        print(self.board)        
-    
+
     def process(self, c):
         """
         c: The single character to process
         """
+        print(self.board)
+        print(c)
         if c in self.board.keys():
-             print(self.board[c]);
+             return(self.board[c]);
         else:
-             print(c);
+             return(c);
 
 plugboard = Plugboard("ABCD")
 plugboard.process('A')
