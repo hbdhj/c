865a2b84d58221607a900b59390416a4be565de7
Deng Haijun <hbdhj@sina.com>
2016-04-27 22:03:51 +0800

    start coding on hackerrank

---
+++ hackerrank/2.c
@@ -0,0 +1,31 @@
+/*
+Simple Array Sum
+
+Sample Input:
+6
+1 2 3 4 10 11
+
+Sample Output:
+31
+*/
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n; 
+    int ret = 0;
+    scanf("%d",&n);
+    int arr[n];
+    for(int arr_i = 0; arr_i < n; arr_i++){
+       scanf("%d",&arr[arr_i]);
+        ret += arr[arr_i];
+    }
+    printf("%d\n", ret);
+    return 0;
+}
