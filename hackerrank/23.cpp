195b67eb79b039b012725d973dd8f262cf36d27d
hbdhj <hbdhj@sina.com>
2016-05-04 20:58:02 +0800

    class pass

---
+++ hackerrank/23.cpp
@@ -0,0 +1,42 @@
+/*
+
+Structs
+
+Sample Input
+
+15
+john
+carmack
+10
+
+Sample Output
+
+15 john carmack 10
+
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+/*
+    add code for struct here.
+*/
+struct Student {
+    int age;
+    string first_name;
+    string last_name;
+    int standard;
+} ;
+
+int main() {
+    Student st;
+    
+    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+    
+    return 0;
+}
