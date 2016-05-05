974b906e0913e43556d95b706f6caebbd6df160f
Deng Haijun <hbdhj@sina.com>
2016-05-05 16:23:49 +0800

    lower bound-stl pass

---
+++ hackerrank/33.cpp
@@ -0,0 +1,49 @@
+/*
+
+Lower Bound-STL
+
+Sample Input
+
+ 8
+ 1 1 2 2 6 9 9 15
+ 4
+ 1
+ 4
+ 9
+ 15
+Sample Output
+
+ Yes 1
+ No 5
+ Yes 6
+ Yes 8
+
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n, y, q;
+    cin>>n;
+    vector<int> x(n);
+    for(int i=0;i<n;i++){
+        cin>>x[i];
+    }
+    cin>>q;
+    for(int i=0;i<q;i++){
+        cin>>y;
+        std::vector<int>::iterator low=std::lower_bound (x.begin(), x.end(), y);
+        if ((*low)==y)
+            cout<<"Yes "<<(low-x.begin()+1)<<endl;
+        else
+            cout<<"No "<<(low-x.begin()+1)<<endl;
+    }
+
+    return 0;
+}
