f1eb5fc7ef804d001c4ee0b1c60ec7d394c9bf13
Deng Haijun <hbdhj@sina.com>
2016-05-14 09:20:03 +0800

    add method - create tree from array

---
+++ leetcode/100_SameTree.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ */
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if ((p==NULL)&&(q==NULL))
+            return true;
+        if (((p!=NULL)&&(q==NULL))|| ((p==NULL)&&(q!=NULL)))
+            return false;
+        if (p->val!=q->val)
+            return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+
+TreeNode* createTreeFromArray(int* arr, int len) {
+    if (sizeof(arr)==0)
+        return NULL;
+    TreeNode** tree = new TreeNode*[len];
+    TreeNode* root = new TreeNode(arr[0]);
+    tree[0] = root;
+    for (int i = 0; i<len; i++) {
+        if (arr[i]!='#')
+            tree[i] = new TreeNode(arr[i]);
+        if (i%2) {
+            tree[(i-1)/2]->left = tree[i];
+        } else {
+            tree[i/2-1]->right = tree[i];
+        }
+    }
+    return root;
+};
+
+int main()
+{
+    int vals[] = {1, 2, 3};
+    TreeNode* root = createTreeFromArray(vals, 3);
+    cout<<root->val<<", "<<root->left->val<<", "<<root->right->val<<endl;
+    return 0;
+}
---
+++ leetcode/101_SymmetricTree.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ */
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool isMirror(TreeNode* left, TreeNode* right) {
+        if ((left==NULL)&&(right==NULL))
+            return true;
+        if (((left==NULL)&&(right!=NULL))||((right==NULL)&&(left!=NULL)))
+            return false;
+        if (left->val!=right->val)
+            return false;
+        return isMirror(left->left, right->right)&&isMirror(left->right, right->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        if (root==NULL)
+            return true;
+        if ((root->left==NULL)&&(root->right==NULL))
+            return true;
+        if (((root->left==NULL)&&(root->right!=NULL))||((root->right==NULL)&&(root->left!=NULL)))
+            return false;
+        return isMirror(root->left, root->right);
+    }
+};
+
+int main()
+{
+    return 0;
+}
