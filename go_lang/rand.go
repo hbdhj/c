6bb9972f99a3ff3160366bfa1d7452d93aa00fd3
Deng Haijun <hbdhj@sina.com>
2016-06-11 21:47:15 +0800

    add a go prog - rand

---
+++ go_lang/rand.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bytes"
+	"crypto/rand"
+	"fmt"
+)
+
+func main() {
+	c := 10
+	b := make([]byte, c)
+	_, err := rand.Read(b)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+	// The slice should now contain random bytes instead of only zeroes.
+	fmt.Println(bytes.Equal(b, make([]byte, c)))
+
+}
