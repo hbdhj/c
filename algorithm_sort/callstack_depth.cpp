8dd72d331ef76e9d9099669a404a92d27da1b4f8
Deng Haijun <hbdhj@sina.com>
2017-05-25 20:52:29 +0800

    call stack depth result

--- algorithm_sort/callstack_depth.cpp
+++ algorithm_sort/callstack_depth.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
-// c++ 4.2.1 Darwin
+// c++ 4.2.1 Darwin max - 174683
+// g++ 4.8.4 Linux 4.2.0 max - 261741
 static int depth = 0;
 
 void func()
 {
     depth++;
     std::cout<<"depth = "<<depth<<std::endl;
+
     func();
 }
 
