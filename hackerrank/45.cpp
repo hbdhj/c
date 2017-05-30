04eed319ecfd90ec64b33a1919c298e6f6a62dca
Deng Haijun <hbdhj@sina.com>
2017-05-30 16:11:27 +0800

    modify hackerrank 45.cpp

--- hackerrank/45.cpp
+++ hackerrank/45.cpp
@@ -1,4 +1,6 @@
 /*
+Copyright 2017 <Deng Haijun>
+
 Artificial Intelligence > Bot Building > Bot saves princess
 
 Sample input
@@ -17,62 +19,56 @@ LEFT
 
 #include <iostream>
 #include <vector>
-using namespace std;
-void displayPathtoPrincess(int n, vector <string> grid)
-{
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+
+void displayPathtoPrincess(int n, vector <string> grid) {
     int px, py, mx, my;
-    for (int i = 0; i<n; i++)
-    {
-        for (int j = 0; j<n; j++)
-        {
-            if (grid[i][j] == 'p')
-            {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (grid[i][j] == 'p') {
                 px = j;
                 py = i;
-            }
-            else if (grid[i][j] == 'm')
-            {
+            } else if (grid[i][j] == 'm') {
                 mx = j;
                 my = i;
             }
         }
     }
-    if (py>my)
-    {
-        for (int i = 0; i<(py - my); i++)
-            cout<<"DOWN"<<endl;
+    if (py > my) {
+        for (int i = 0; i < (py - my); i++)
+            cout << "DOWN" << endl;
+    } else if (my > py) {
+        for (int i = 0; i < (my - py); i++)
+            cout << "UP" << endl;
     }
-    else if (my>py)
-    {
-        for (int i = 0; i<(my - py); i++)
-            cout<<"UP"<<endl;
-    }
-    if (px>mx)
-    {
-        for (int i = 0; i<(px - mx); i++)
-            cout<<"RIGHT"<<endl;
-    }
-    else if (mx>px)
-    {
-        for (int i = 0; i<(mx - px); i++)
-            cout<<"LEFT"<<endl;
+    if (px > mx) {
+        for (int i = 0; i < (px - mx); i++)
+            cout << "RIGHT" << endl;
+    } else if (mx > px) {
+        for (int i = 0; i < (mx - px); i++)
+            cout << "LEFT" << endl;
     }
 }
 
-int main(void)
-{
+int main(void) {
     int m;
     vector <string> grid;
 
     cin >> m;
 
-    for(int i=0; i<m; i++)
-    {
-        string s; cin >> s;
+    for (int i = 0; i < m; i++) {
+        string s;
+        cin >> s;
         grid.push_back(s);
     }
 
-    displayPathtoPrincess(m,grid);
+    displayPathtoPrincess(m, grid);
 
     return 0;
 }
