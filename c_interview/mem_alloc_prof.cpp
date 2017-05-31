8199f7cad987de00d257282d8817f3171741f9ee
Haijun Deng <hbdhj@sina.com>
2017-05-31 23:16:47 +0800

    fix mem_alloc_prof.cpp error

--- c_interview/mem_alloc_prof.cpp
+++ c_interview/mem_alloc_prof.cpp
@@ -14,7 +14,7 @@ int main() {
     int r = 10;
     int c = 1000;
     int s = 100;
-    vector<vector<int*> > matrix1(r)
+    vector<vector<int*> > matrix1(r);
     for (int i = 0; i < r; i++) {
         matrix1[i].resize(c);
         for (int j = 0; j < c; j++) {
@@ -26,15 +26,15 @@ int main() {
             delete matrix1[i][j];
         }
     }
-    vector<vector<int*> > matrix2(c)
+    vector<vector<int*> > matrix2(c);
     for (int i = 0; i < c; i++) {
         matrix2[i].resize(r);
         for (int j = 0; j < r; j++) {
             matrix2[i][j] = new int[s];
         }
     }
-    for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++) {
+    for (int i = 0; i < c; i++) {
+        for (int j = 0; j < r; j++) {
             delete matrix2[i][j];
         }
     }
