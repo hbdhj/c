8a303d01df2a876489be948e1c5cd0876e2354f7
Deng Haijun <hbdhj@sina.com>
2017-06-05 21:29:09 +0800

    hackerrank > Tutorials > Cracking the Coding Interview > Arrays: Left Rotation

---
+++ hackerrank/58.cpp
@@ -0,0 +1,41 @@
+/*
+
+Copyright 2017 <Deng Haijun>
+
+Tutorials > Cracking the Coding Interview > Arrays: Left Rotation 
+
+Sample Input
+
+5 4
+1 2 3 4 5
+
+Sample Output
+
+5 1 2 3 4
+
+*/
+
+#include <vector>
+#include <iostream>
+
+
+using namespace std;
+
+vector<int> array_left_rotation(vector<int> a, int n, int k) {
+    
+}
+
+int main(){
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(int a_i = 0;a_i < n;a_i++){
+        cin >> a[a_i];
+    }
+    vector<int> output = array_left_rotation(a, n, k);
+    for(int i = 0; i < n;i++)
+        cout << output[i] << " ";
+    cout << endl;
+    return 0;
+}
