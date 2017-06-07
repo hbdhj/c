be75a706ee4b332569382247f4fb95c712b96f90
Deng Haijun <hbdhj@sina.com>
2017-06-07 20:16:05 +0800

    hackerrank > Tutorials > 10 Days of Statistics > Day 0: Weighted Mean pass

---
+++ hackerrank/63.cpp
@@ -0,0 +1,39 @@
+/*
+
+Copyright 2017 <Deng Haijun>
+
+Tutorials > 10 Days of Statistics > Day 0: Weighted Mean
+
+Sample Input
+
+5
+10 40 30 50 20
+1 2 3 4 5
+
+Sample Output
+
+32.0
+
+*/
+
+#include "./common.h"
+
+int main() {
+    int n, wi;
+    cin >> n;
+    vector<int> xis(n);
+    int weight_total = 0;
+    int value_total = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> xis[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> wi;
+        value_total += wi * xis[i];
+        weight_total += wi;
+    }
+    
+    printf("%.1f\n", float(value_total) /weight_total);
+    
+    return 0;
+}
