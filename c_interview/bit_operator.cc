8aa822ee8f626b32d909fc9566c6d146ce740a5f
hbdhj <hbdhj@sina.com>
2017-06-09 11:53:03 +0600

    cpplint bit_operator.cc

---
+++ c_interview/bit_operator.cc
@@ -0,0 +1,22 @@
+/*
+
+Copyright 2017 <Deng Haijun>
+
+*/
+
+#include <stdio.h>
+#include <arpa/inet.h>
+
+
+int main() {
+    short i = 0xa027;
+    printf("sizeof(short) = %d, 0x%x & 0xff = 0x%x\n",
+           sizeof(short), i, 0xff & i);
+    printf("3&2 = %d, 3&1 = %d, 2&2 = %d, 2&1 = %d, 1&1 = %d\n",
+           3&2, 3&1, 2&2, 2&1, 1&1);
+    printf("3|2 = %d, 3|1 = %d, 2|2 = %d, 2|1 = %d, 1|1 = %d\n",
+           3|2, 3|1, 2|2, 2|1, 1|1);
+    printf("3^2 = %d, 3^1 = %d, 2^2 = %d, 2^1 = %d, 1^1 = %d\n",
+           3^2, 3^1, 2^2, 2^1, 1^1);
+    return 0;
+}
