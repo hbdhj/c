053c1f0242cdddc0c3e4099e74a6081ac81d5474
hbdhj <hbdhj@sina.com>
2017-07-07 18:19:19 +0600

    ptrace example 1

---
+++ c_interview/ptrace_example1.cc
@@ -0,0 +1,26 @@
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/user.h>
+#include <sys/reg.h>   /* For constants ORIG_RAX etc */
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+    pid_t child;
+    long orig_rax;
+    child = fork();
+    if(child == 0) {
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        execl("/bin/ls", "ls", NULL);
+    } else {
+        wait(NULL);
+        orig_rax = ptrace(PTRACE_PEEKUSER, 
+                          child, 4 * ORIG_RAX, 
+                          NULL);
+        printf("The child made a "
+               "system call %ld \n", orig_rax);
+        ptrace(PTRACE_CONT, child, NULL, NULL);
+    }
+    return 0;
+}
