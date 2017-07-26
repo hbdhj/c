d374e8cf14533719365e7081ac090f7e5059732a
hbdhj <hbdhj@sina.com>
2017-07-26 10:32:32 +0600

    enable commit automatically

--- shell/code_lines_count.sh
+++ shell/code_lines_count.sh
@@ -2,14 +2,22 @@
 
 cloc . > cloc_result.xml
 
-lang_list=`head -23 cloc_result.xml | tail -15 | awk '{print "\x27" $1 "\x27" }'`
-echo "var myLang = new Array("$lang_list");" >  hbdhj.github.io/resume/codeline_data.js
+echo "// update at `date`" > codeline_data.js
 
-line_list=`head -23 cloc_result.xml | tail -15 | awk '{print $5}'`
-echo "var myLines = new Array("$line_list");" >>  hbdhj.github.io/resume/codeline_data.js
+lang_list=`head -23 cloc_result.xml | tail -15 | awk '{print "\x27" $1 "\x27" "\x2c" }'`
+echo "var myLang = new Array("$lang_list");" >>  codeline_data.js
 
-rm cloc_result.xml
+line_list=`head -23 cloc_result.xml | tail -15 | awk '{print $5 "\x2c"}'`
+echo "var myLines = new Array("$line_list");" >>  codeline_data.js
 
-cd hbdhj.github.io
-git add .
-git commit -m "update at `date`"
+#rm cloc_result.xml
+
+diff codeline_data.js hbdhj.github.io/resume/codeline_data.js
+if [ $? -eq 1 ]; then
+	cp codeline_data.js hbdhj.github.io/resume/codeline_data.js
+	cd hbdhj.github.io
+	git add .
+	if [ $? -eq 0 ]; then
+		git commit -m "update at `date`"
+	fi
+fi
