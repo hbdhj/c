539703863807608e4f14e89779d6565168287752
hbdhj <hbdhj@sina.com>
2017-08-21 10:52:03 +0600

    fix code line coutner bug

--- shell/code_lines_count.sh
+++ shell/code_lines_count.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cloc . > cloc_result.xml
+cloc --csv . > cloc_result.xml
 
 echo "// update at `date`" > codeline_data.js
 
-lang_list=`head -23 cloc_result.xml | tail -15 | awk '{print "\x27" $1 "\x27" "\x2c" }'`
+lang_list=`head -20 cloc_result.xml | tail -15 | awk -F, '{print "\x27" $2 "\x27" "\x2c" }'`
 echo "var myLang = new Array("$lang_list");" >>  codeline_data.js
 
-line_list=`head -23 cloc_result.xml | tail -15 | awk '{print $5 "\x2c"}'`
+line_list=`head -20 cloc_result.xml | tail -15 | awk -F, '{print $5 "\x2c"}'`
 echo "var myLines = new Array("$line_list");" >>  codeline_data.js
 
 #rm cloc_result.xml
