8179aec3710116c7379bc2e5bd6b87b643b625a3
Deng Haijun <hbdhj@sina.com>
2017-10-18 22:04:55 +0800

    update ebbchar linux module

--- Linux/kernel/ebbchar/ebbchar.c
+++ Linux/kernel/ebbchar/ebbchar.c
@@ -1,33 +1,33 @@
 /**
- * @file   ebbchar.c
+ * @file    ebbchar.c
  * @author Derek Molloy
- * @date   7 April 2015
+ * @date    7 April 2015
  * @version 0.1
- * @brief   An introductory character driver to support the second article of my series on
+ * @brief    An introductory character driver to support the second article of my series on
  * Linux loadable kernel module (LKM) development. This module maps to /dev/ebbchar and
  * comes with a helper C program that can be run in Linux user space to communicate with
  * this the LKM.
  * @see http://www.derekmolloy.ie/ for a full description and follow-up descriptions.
  */
  
-#include <linux/init.h>           // Macros used to mark up functions e.g. __init __exit
-#include <linux/module.h>         // Core header for loading LKMs into the kernel
-#include <linux/device.h>         // Header to support the kernel Driver Model
-#include <linux/kernel.h>         // Contains types, macros, functions for the kernel
-#include <linux/fs.h>             // Header for the Linux file system support
-#include <asm/uaccess.h>          // Required for the copy to user function
-#define  DEVICE_NAME "ebbchar"    ///< The device will appear at /dev/ebbchar using this value
-#define  CLASS_NAME  "ebb"        ///< The device class -- this is a character device driver
+#include <linux/init.h>              // Macros used to mark up functions e.g. __init __exit
+#include <linux/module.h>            // Core header for loading LKMs into the kernel
+#include <linux/device.h>            // Header to support the kernel Driver Model
+#include <linux/kernel.h>            // Contains types, macros, functions for the kernel
+#include <linux/fs.h>                // Header for the Linux file system support
+#include <asm/uaccess.h>             // Required for the copy to user function
+#define  DEVICE_NAME "ebbchar"       ///< The device will appear at /dev/ebbchar using this value
+#define  CLASS_NAME  "ebb"           ///< The device class -- this is a character device driver
  
-MODULE_LICENSE("GPL");            ///< The license type -- this affects available functionality
-MODULE_AUTHOR("Derek Molloy");    ///< The author -- visible when you use modinfo
+MODULE_LICENSE("GPL");               ///< The license type -- this affects available functionality
+MODULE_AUTHOR("Deng Haijun");        ///< The author -- visible when you use modinfo
 MODULE_DESCRIPTION("A simple Linux char driver for the BBB");  ///< The description -- see modinfo
-MODULE_VERSION("0.1");            ///< A version number to inform users
+MODULE_VERSION("0.1");               ///< A version number to inform users
  
-static int    majorNumber;                  ///< Stores the device number -- determined automatically
-static char   message[256] = {0};           ///< Memory for the string that is passed from userspace
+static int     majorNumber;                 ///< Stores the device number -- determined automatically
+static char    message[256] = {0};          ///< Memory for the string that is passed from userspace
 static short  size_of_message;              ///< Used to remember the size of the string stored
-static int    numberOpens = 0;              ///< Counts the number of times the device is opened
+static int     numberOpens = 0;             ///< Counts the number of times the device is opened
 static struct class*  ebbcharClass  = NULL; ///< The device-driver class struct pointer
 static struct device* ebbcharDevice = NULL; ///< The device-driver device struct pointer
  
@@ -43,10 +43,10 @@ static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
  */
 static struct file_operations fops =
 {
-   .open = dev_open,
-   .read = dev_read,
-   .write = dev_write,
-   .release = dev_release,
+    .open = dev_open,
+    .read = dev_read,
+    .write = dev_write,
+    .release = dev_release,
 };
  
 /** @brief The LKM initialization function
@@ -56,35 +56,35 @@ static struct file_operations fops =
  *  @return returns 0 if successful
  */
 static int __init ebbchar_init(void){
-   printk(KERN_INFO "EBBChar: Initializing the EBBChar LKM\n");
+    printk(KERN_INFO "EBBChar: Initializing the EBBChar LKM\n");
  
-   // Try to dynamically allocate a major number for the device -- more difficult but worth it
-   majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
-   if (majorNumber<0){
-      printk(KERN_ALERT "EBBChar failed to register a major number\n");
-      return majorNumber;
-   }
-   printk(KERN_INFO "EBBChar: registered correctly with major number %d\n", majorNumber);
+    // Try to dynamically allocate a major number for the device -- more difficult but worth it
+    majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
+    if (majorNumber<0){
+        printk(KERN_ALERT "EBBChar failed to register a major number\n");
+        return majorNumber;
+    }
+    printk(KERN_INFO "EBBChar: registered correctly with major number %d\n", majorNumber);
  
-   // Register the device class
-   ebbcharClass = class_create(THIS_MODULE, CLASS_NAME);
-   if (IS_ERR(ebbcharClass)){                // Check for error and clean up if there is
-      unregister_chrdev(majorNumber, DEVICE_NAME);
-      printk(KERN_ALERT "Failed to register device class\n");
-      return PTR_ERR(ebbcharClass);          // Correct way to return an error on a pointer
-   }
-   printk(KERN_INFO "EBBChar: device class registered correctly\n");
+    // Register the device class
+    ebbcharClass = class_create(THIS_MODULE, CLASS_NAME);
+    if (IS_ERR(ebbcharClass)){                    // Check for error and clean up if there is
+        unregister_chrdev(majorNumber, DEVICE_NAME);
+        printk(KERN_ALERT "Failed to register device class\n");
+        return PTR_ERR(ebbcharClass);             // Correct way to return an error on a pointer
+    }
+    printk(KERN_INFO "EBBChar: device class registered correctly\n");
  
-   // Register the device driver
-   ebbcharDevice = device_create(ebbcharClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
-   if (IS_ERR(ebbcharDevice)){               // Clean up if there is an error
-      class_destroy(ebbcharClass);           // Repeated code but the alternative is goto statements
-      unregister_chrdev(majorNumber, DEVICE_NAME);
-      printk(KERN_ALERT "Failed to create the device\n");
-      return PTR_ERR(ebbcharDevice);
-   }
-   printk(KERN_INFO "EBBChar: device class created correctly\n"); // Made it! device was initialized
-   return 0;
+    // Register the device driver
+    ebbcharDevice = device_create(ebbcharClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
+    if (IS_ERR(ebbcharDevice)){                   // Clean up if there is an error
+        class_destroy(ebbcharClass);              // Repeated code but the alternative is goto statements
+        unregister_chrdev(majorNumber, DEVICE_NAME);
+        printk(KERN_ALERT "Failed to create the device\n");
+        return PTR_ERR(ebbcharDevice);
+    }
+    printk(KERN_INFO "EBBChar: device class created correctly\n"); // Made it! device was initialized
+    return 0;
 }
  
 /** @brief The LKM cleanup function
@@ -92,11 +92,11 @@ static int __init ebbchar_init(void){
  *  code is used for a built-in driver (not a LKM) that this function is not required.
  */
 static void __exit ebbchar_exit(void){
-   device_destroy(ebbcharClass, MKDEV(majorNumber, 0));     // remove the device
-   class_unregister(ebbcharClass);                          // unregister the device class
-   class_destroy(ebbcharClass);                             // remove the device class
-   unregister_chrdev(majorNumber, DEVICE_NAME);             // unregister the major number
-   printk(KERN_INFO "EBBChar: Goodbye from the LKM!\n");
+    device_destroy(ebbcharClass, MKDEV(majorNumber, 0));      // remove the device
+    class_unregister(ebbcharClass);                           // unregister the device class
+    class_destroy(ebbcharClass);                              // remove the device class
+    unregister_chrdev(majorNumber, DEVICE_NAME);              // unregister the major number
+    printk(KERN_INFO "EBBChar: Goodbye from the LKM!\n");
 }
  
 /** @brief The device open function that is called each time the device is opened
@@ -105,9 +105,9 @@ static void __exit ebbchar_exit(void){
  *  @param filep A pointer to a file object (defined in linux/fs.h)
  */
 static int dev_open(struct inode *inodep, struct file *filep){
-   numberOpens++;
-   printk(KERN_INFO "EBBChar: Device has been opened %d time(s)\n", numberOpens);
-   return 0;
+    numberOpens++;
+    printk(KERN_INFO "EBBChar: Device has been opened %d time(s)\n", numberOpens);
+    return 0;
 }
  
 /** @brief This function is called whenever device is being read from user space i.e. data is
@@ -119,18 +119,18 @@ static int dev_open(struct inode *inodep, struct file *filep){
  *  @param offset The offset if required
  */
 static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset){
-   int error_count = 0;
-   // copy_to_user has the format ( * to, *from, size) and returns 0 on success
-   error_count = copy_to_user(buffer, message, size_of_message);
+    int error_count = 0;
+    // copy_to_user has the format ( * to, *from, size) and returns 0 on success
+    error_count = copy_to_user(buffer, message, size_of_message);
  
-   if (error_count==0){            // if true then have success
-      printk(KERN_INFO "EBBChar: Sent %d characters to the user\n", size_of_message);
-      return (size_of_message=0);  // clear the position to the start and return 0
-   }
-   else {
-      printk(KERN_INFO "EBBChar: Failed to send %d characters to the user\n", error_count);
-      return -EFAULT;              // Failed -- return a bad address message (i.e. -14)
-   }
+    if (error_count==0){                // if true then have success
+        printk(KERN_INFO "EBBChar: Sent %d characters to the user\n", size_of_message);
+        return (size_of_message=0);  // clear the position to the start and return 0
+    }
+    else {
+        printk(KERN_INFO "EBBChar: Failed to send %d characters to the user\n", error_count);
+        return -EFAULT;                  // Failed -- return a bad address message (i.e. -14)
+    }
 }
  
 /** @brief This function is called whenever the device is being written to from user space i.e.
@@ -142,10 +142,10 @@ static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *of
  *  @param offset The offset if required
  */
 static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset){
-   sprintf(message, "%s(%zu letters)", buffer, len);   // appending received string with its length
-   size_of_message = strlen(message);                 // store the length of the stored message
-   printk(KERN_INFO "EBBChar: Received %zu characters from the user\n", len);
-   return len;
+    sprintf(message, "%s(%zu letters)", buffer, len);    // appending received string with its length
+    size_of_message = strlen(message);                   // store the length of the stored message
+    printk(KERN_INFO "EBBChar: Received %zu characters from the user\n", len);
+    return len;
 }
  
 /** @brief The device release function that is called whenever the device is closed/released by
@@ -154,8 +154,8 @@ static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, lof
  *  @param filep A pointer to a file object (defined in linux/fs.h)
  */
 static int dev_release(struct inode *inodep, struct file *filep){
-   printk(KERN_INFO "EBBChar: Device successfully closed\n");
-   return 0;
+    printk(KERN_INFO "EBBChar: Device successfully closed\n");
+    return 0;
 }
  
 /** @brief A module must use the module_init() module_exit() macros from linux/init.h, which
