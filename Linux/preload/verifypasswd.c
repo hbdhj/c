70721900331c2d5fb128df2f871d0ebd5b58c521
hbdhj <hbdhj@sina.com>
2018-03-26 13:20:32 +0600

    verify password

--- Linux/preload/verifypasswd.c
+++ Linux/preload/verifypasswd.c
@@ -3,18 +3,19 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
     char passwd[] = "password";
     if (argc < 2) {
         printf("usage: %s <password>/n", argv[0]);
-        return;
+        exit(1);
     }
 
     if (!strcmp(passwd, argv[1])) {
         printf("Correct Password!/n");
-        return;
+        exit(2);
     }
 
     printf("Invalid Password!/n");
