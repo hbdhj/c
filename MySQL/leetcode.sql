90ceec0af6c4405e37e9617bb7189c35a1d35d63
Deng Haijun <hbdhj@sina.com>
2018-03-29 21:04:36 +0800

    some update on leetcode sql

--- MySQL/leetcode.sql
+++ MySQL/leetcode.sql
@@ -3,7 +3,7 @@
 SELECT FirstName, LastName, City, State FROM Person LEFT OUTER JOIN Address ON Person.PersonId = Address.PersonId;
 
 -- 176. Second Highest Salary
-SELECT MAX(Salary) FROM Employee WHERE Salary <> (  SELECT MAX(Salary) FROM Employee);
+SELECT MAX(Salary) FROM Employee WHERE Salary <> (SELECT MAX(Salary) FROM Employee);
 
 -- 181. Employees Earning More Than Their Managers
 SELECT Name FROM Employee AS A WHERE Salary>(SELECT Salary FROM Employee AS B WHERE Id = A.ManagerId);
@@ -19,3 +19,12 @@ SELECT w1.Id FROM Weather w1, Weather w2 WHERE w2.Date = DATE_SUB(w1.Date, INTER
 
 -- 196. Delete Duplicate Emails
 DELETE P2 FROM Person AS P1, Person AS P2 WHERE  P1.Email = P2.Email AND P1.Id < P2.Id;
+
+-- 596. Classes More Than 5 Students
+SELECT class FROM (SELECT class, COUNT(DISTINCT student) AS num FROM courses GROUP BY class) AS temp_table WHERE num >= 5;
+
+-- 620. Not Boring Movies
+select * from cinema where mod(id, 2) = 1 and description != 'boring' order by rating DESC;
+
+-- 627. Swap Salary
+UPDATE salary SET sex = CASE sex WHEN 'm' THEN 'f' ELSE 'm' END;
